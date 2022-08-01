2b60517402ae2398ff3b5f31ac5168f97e6ef5c3
Mikey Lee <mikey1384@gmail.com>
2022-08-01 01:47:43 +0000

    implement header layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import './App.css';
+import logo from './logo.png';
 
 function App() {
   return (
-    <main>
-      <header>this is the header</header>
-    </main>
+    <div>
+      <header>
+        <div style={{display: 'flex', alignItems: 'center'}}>
+          <img style={{ height: '40px', width: '27px' }} src={logo} />
+          <span
+            style={{
+              fontFamily: 'Oswald',
+              marginLeft: '1rem' }}
+          >
+            Anthropocene
+          </span>
+        </div>
+        <div style={{display: 'flex', alignItems: 'center'}}>
+          <nav>Home</nav>
+          <nav style={{marginLeft: '1rem'}}>Corners</nav>
+          <nav style={{marginLeft: '1rem'}}>About Us</nav>
+        </div>
+      </header>
+      <main>
+        this is main
+      </main>
+    </div>
   );
 }
 
